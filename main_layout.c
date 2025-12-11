9db20d387dbc2da261c0acd5b3d72f306432793b
Yuval Herman <yuvalherman99@gmail.com>
2025-12-11 22:50:59 +0200

    highlight selected cell

--- main_layout.c
+++ main_layout.c
@@ -22,9 +22,29 @@ Texture2D* char2tex(ChessTextures *tex, char c) {
   }
 }
 
+typedef struct {
+  int *selected_row;
+  int *selected_col;
+} BoardHoverData;
+
+void handle_board_cell_hover(Clay_ElementId element_id,
+                             Clay_PointerData pointer_data, void *user_data) {
+  BoardHoverData *click_data = user_data;
+  if (pointer_data.state == CLAY_POINTER_DATA_PRESSED_THIS_FRAME) {
+    int col = element_id.offset % 8;
+    int row = (element_id.offset - col) / 8;
+    *click_data->selected_col = col;
+    *click_data->selected_row = row;
+  }
+}
+
 void board_layout(ChessTextures *chess_pieces, char board[8][8]) {
   Clay_Color even_cell_color = {100, 100, 100, 255};
   Clay_Color odd_cell_color  = {125, 125, 125, 255};
+  static int selected_row = -1;
+  static int selected_col = -1;
+  static BoardHoverData click_data = {&selected_row, &selected_col};
+
   CLAY(CLAY_ID("BoardContainer"), {
        .layout = {
           .layoutDirection = CLAY_TOP_TO_BOTTOM,
@@ -50,7 +70,7 @@ void board_layout(ChessTextures *chess_pieces, char board[8][8]) {
         }) {
         for (int col = 0; col < 8; col++) {
           Clay_Color cell_color = (row + col) % 2 ? odd_cell_color : even_cell_color;
-          CLAY_AUTO_ID({
+          CLAY(CLAY_IDI("cell", col + row * 8),{
                 .layout = {
                   .sizing = {
                     .width = CLAY_SIZING_GROW(),
@@ -58,9 +78,10 @@ void board_layout(ChessTextures *chess_pieces, char board[8][8]) {
                     }
                   },
                 .cornerRadius = CLAY_CORNER_RADIUS(4),
-                .backgroundColor = cell_color,
+                .backgroundColor = (Clay_Hovered() || (row == selected_row && col == selected_col)) ? (Clay_Color){125,125,100,255} : cell_color,
                 .aspectRatio = {.aspectRatio = 1}
           }) {
+            Clay_OnHover(handle_board_cell_hover, &click_data);
             CLAY_AUTO_ID({
               .layout = {
                 .sizing = {
